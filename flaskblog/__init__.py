15cab5e0824f4a1af6d49c58a7b6d80ebe95f46d
silktreesolutions <joel.r.julian0408@gmail.com>
2020-02-20 16:28:13 +0530

    Added flask bcrypt and login manager

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f5707cb7053ec45aeaff95d7c6f8999f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:admin@localhost:5432/flaskblog'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
+
